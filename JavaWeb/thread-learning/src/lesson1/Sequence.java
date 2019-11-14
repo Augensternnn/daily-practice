38f31470a8fb5ab33d4839fd5555acd8ec7a58af
Augenstern <1024605927@qq.com>
2019-11-14 08:13:56 +0800

    add

--- JavaWeb/thread-learning/src/lesson1/Sequence.java
+++ JavaWeb/thread-learning/src/lesson1/Sequence.java
@@ -3,6 +3,25 @@ package lesson1;
 public class Sequence {
     // 启动 0-9 编号的10个线程，每个线程打印他的编号
     public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            final int j = i;
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    // 启动0-9编号的10个线程，每个线程打印他的编号
+                    try {
+                        Thread.sleep(1000*j);
+                        System.out.println(j);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            Thread t = new Thread(r);
+            t.start();
+        }
+
+        /*
         for (int i = 0; i < 10; i++) {
             final int j = i;
             Runnable r = new Runnable() {
@@ -14,5 +33,6 @@ public class Sequence {
             Thread t = new Thread(r);
             t.start();
         }
+         */
     }
 }
