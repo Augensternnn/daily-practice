a9cf9308f7ae64ad55e3acb4e41e5b55ddb7f670
Augenstern <1024605927@qq.com>
2019-11-16 13:12:47 +0800

    add

--- JavaWeb/thread-learning/src/lesson2/MyThread.java
+++ JavaWeb/thread-learning/src/lesson2/MyThread.java
@@ -1,14 +1,18 @@
 package lesson2;
 
 public class MyThread {
-    public static void main(String[] args) {
-        new Thread(new Runnable() {
+    public static void main(String[] args) throws InterruptedException{
+        Thread thread =new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("啦啦啦啦啦");
             }
         });
-
+        thread.start();
+        // 不调用join时，创建线程会耗时较长，下边的main方法的代码会先执行
+        // 如果调用join，表示thread（线程的引用）会加入当前线程（JavaMain主线程），等待thread执行完毕再执行后边代码
+        thread.join();      // 阻塞方法
+        System.out.println("哈哈哈哈哈");
         /*
         new Thread(new Runnable() {
             @Override
