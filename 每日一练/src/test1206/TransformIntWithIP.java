80cf6c65c934a19f07cccc2b0e12067176c73cfa
Augenstern <1024605927@qq.com>
2019-12-07 17:08:34 +0800

    add

--- 每日一练/src/test1206/TransformIntWithIP.java
+++ 每日一练/src/test1206/TransformIntWithIP.java
@@ -9,26 +9,41 @@ public class TransformIntWithIP {
 //        String str = in.next();
 //        int i = in.nextInt();
         String str = "10.0.3.193";
-        int n = 167969729;
+        int ip = 167969729;
         String[] s = str.split("\\.");
         DecimalFormat df = new DecimalFormat("00000000");
         String res = "";
         for(int j = 0; j < s.length; j++ ){
             res = res + df.format(Integer.parseInt(Integer.toBinaryString(Integer.parseInt(s[j]))));
         }
-        long ll = 0;
+        long result1 = 0;
         for(int l = res.length()-1; l >= 0; l--){
-            ll += (res.charAt(l)-'0')*Math.pow(2,res.length()-l-1);
+            result1 += (res.charAt(l)-'0')*Math.pow(2,res.length()-l-1);
         }
-        System.out.println(ll);
-        String ii = Integer.toBinaryString(n);
+        System.out.println(result1);
+        String str2 = Integer.toBinaryString(ip);
         int[] ar = new int[4];
-        int mm = 0;
-        for(int k = ii.length(); k >= 0; k --){
-            String ss = df.format(ii.substring(k-8, k));
-            for(int jj = 0; jj < ss.length(); jj++){
-
+        int jj = 0;
+        int index = str2.length();
+        while (jj < 4){
+            String ss = null;
+            if(str2.substring(0, index).length() >= 8) {
+                ss = str2.substring(index - 8, index);
+            }else {
+                ss = str2.substring(0, index);
             }
+            int ii = 0;
+            for(int m = ss.length()-1; m >= 0; m--){
+                ii += (ss.charAt(m)-'0')*Math.pow(2, ss.length()-m-1);
+            }
+            ar[jj++] = ii;
+            index -= 8;
+        }
+        for(int i = ar.length-1; i >= 0; i--){
+            if(ar[i] == ar[0])
+                System.out.print(ar[0]);
+            else
+                System.out.print(ar[i] + ".");
         }
     }
 }
