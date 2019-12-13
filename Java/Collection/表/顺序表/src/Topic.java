49ed8dfe437d3f03086237f45508286d01b8ece0
Augenstern <1024605927@qq.com>
2019-12-14 07:25:17 +0800

    add

--- Java/Collection/表/顺序表/src/Topic.java
+++ Java/Collection/表/顺序表/src/Topic.java
@@ -87,8 +87,71 @@ class Solution{
         }
         return fakeHead.next;
     }
-
+    public ListNode removeElements2(ListNode head, int val) {
+        ListNode newList = null;
+        ListNode last = null;       //  记录 newList 最后一个结点
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            if(cur.val != val){
+                // 把 cur 尾插到 newList
+                if(newList == null){    // 第一次尾插时，newList 空链表
+                    cur.next = newList;
+                    newList = cur;
+                }else {     // 以后尾插时，newList 不是空链表
+                    last.next = next;
+                }
+                last = cur;     // 负责更新 last，保证 last 始终是最后一个结点
+            }
+            cur = next;
+        }
+        if(last != null){       // 处理 head == null 的情况
+            last.next = null;   // 保证了最后一个结点的 next == null
+        }
+        return newList;
+    }
 }
 public class Topic {
+    public static void display(ListNode head) {
+        for (ListNode n = head; n != null; n = n.next) {
+            System.out.print(n.val + " ");
+        }
+        System.out.println();
+    }
 
+    public static void testReverseList() {
+        System.out.println("测试反转链表");
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
+
+        Solution s = new Solution();
+        ListNode result = s.reverseList(n1);
+        // 打印 result
+        display(result);
+    }
+
+    public static void testRemoveElements() {
+        System.out.println("测试删除元素");
+        ListNode n1 = new ListNode(6);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(6);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(6);
+
+        n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
+        Solution s = new Solution();
+        ListNode result = s.removeElements(n1, 6);
+        // 打印 result
+        display(result);
+    }
+
+    public static void main(String[] args) {
+        testReverseList();
+        testRemoveElements();
+    }
 }
