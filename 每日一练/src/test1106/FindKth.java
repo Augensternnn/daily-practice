c3da7b5d1148a224fac7025a97b5ffce9c38513b
Augenstern <1024605927@qq.com>
2019-12-15 07:30:38 +0800

    add

--- 每日一练/src/test1106/FindKth.java
+++ 每日一练/src/test1106/FindKth.java
@@ -4,38 +4,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 有一个整数数组，根据快排的思路，找出数组找中第K大的数
- * 给定一个整数数组a，同时给定它的大小n和要找的K(k在1到n之间)，请返回第k大的数，保证答案存在
+ * https://www.nowcoder.com/questionTerminal/e016ad9b7f0b45048c58a9f27ba618bf
  */
 public class FindKth {
-    public int findKth(int[] a, int n, int k){
-        return findKth(a, 0, n-1, k);
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 2, 2};
+        System.out.println(findKth(arr, 5, 3));
     }
-    public int findKth(int[] a, int low, int high, int k){
-        int part = partition(a, low, high);
-        if(k == part - low + 1){
-            return a[part];
-        }else if(k > part - low + 1){
-            return findKth(a, part+1, high, k-part+low-1);
-        }else {
-            return findKth(a, low, part-1, k);
-        }
+    public static int findKth(int[] arr, int n, int k){
+        
     }
-    public int partition(int[] a, int low, int high){
-        int key = a[low];
-        while (low < high){
-            while (low < high && a[high] <= key){
-                high--;
-            }
-            a[low] = a[high];
-            while (low < high && a[low] >= key){
-                low++;
-            }
-            a[high] = a[low];
-        }
-        a[low] = key;
-        return low;
-    }
+//    public int findKth(int[] a, int n, int k){
+//        return findKth(a, 0, n-1, k);
+//    }
+//    public int findKth(int[] a, int low, int high, int k){
+//        int part = partition(a, low, high);
+//        if(k == part - low + 1){
+//            return a[part];
+//        }else if(k > part - low + 1){
+//            return findKth(a, part+1, high, k-part+low-1);
+//        }else {
+//            return findKth(a, low, part-1, k);
+//        }
+//    }
+//    public int partition(int[] a, int low, int high){
+//        int key = a[low];
+//        while (low < high){
+//            while (low < high && a[high] <= key){
+//                high--;
+//            }
+//            a[low] = a[high];
+//            while (low < high && a[low] >= key){
+//                low++;
+//            }
+//            a[high] = a[low];
+//        }
+//        a[low] = key;
+//        return low;
+//    }
     /*
     public static int findKth(int[] array, int n, int k){
         Arrays.sort(array);
--- 每日一练/src/test1106/Palindrome.java
+++ 每日一练/src/test1106/Palindrome.java
@@ -1,68 +1,68 @@
 package test1106;
 /**
- * 每组输入数据共两行：第一行为字符串A，第二行为字符串B，字符串长度均小于100且只包含小写字母
- * 输出一个数字，表示把字符串B插入字符串A之后构成一个回文串的方法数
+ * https://www.nowcoder.com/questionTerminal/9d1559511b3849deaa71b576fa7009dc
  */
 
 import java.util.Scanner;
 
 public class Palindrome {
-    public static boolean isPalindrome(String s, int i, int j){
-        while (i <= j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        String b = in.nextLine();
-        int count = 0;
-        if(isPalindrome(a+b, 0, a.length()+b.length()-1)){
-            count++;
-        }
-        if(isPalindrome(b+a, 0, a.length()+b.length()-1)){
-            count++;
-        }
-        for(int i = 1; i < a.length(); i++){
-            if(isPalindrome(a.substring(0, i)+b+a.substring(i), 0, a.length()+b.length()-1)){
-                count++;
+        while (in.hasNext()){
+            String a = in.nextLine();
+            String b = in.nextLine();
+            int count = 0;
+            for(int i = 0; i <= a.length(); i++){
+                StringBuilder sb = new StringBuilder(a);
+                sb.insert(i, b);
+                if(isPalindrome(sb.toString()))
+                    count++;
             }
+            System.out.println(count);
         }
-        System.out.println(count);
     }
-
-     /*
     public static boolean isPalindrome(String s){
         int i = 0;
-        int j = s.length() - 1;
+        int j = s.length()-1;
         while (i < j){
-            if(s.charAt(i) != s.charAt(j)){
+            if(s.charAt(i) != s.charAt(j))
                 return false;
-            }
             i++;
             j--;
         }
         return true;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        String b = sc.nextLine();
-        int count = 0;
-        for(int i = 0; i <= a.length(); i++){
-            StringBuilder sb = new StringBuilder(a);
-            sb.insert(i, b);
-            if(isPalindrome(sb.toString())){
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
 
-      */
+
+
+
+//    public static boolean isPalindrome(String s, int i, int j){
+//        while (i <= j) {
+//            if (s.charAt(i) != s.charAt(j)) {
+//                return false;
+//            }
+//            i++;
+//            j--;
+//        }
+//        return true;
+//    }
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        String a = in.nextLine();
+//        String b = in.nextLine();
+//        int count = 0;
+//        if(isPalindrome(a+b, 0, a.length()+b.length()-1)){
+//            count++;
+//        }
+//        if(isPalindrome(b+a, 0, a.length()+b.length()-1)){
+//            count++;
+//        }
+//        for(int i = 1; i < a.length(); i++){
+//            if(isPalindrome(a.substring(0, i)+b+a.substring(i), 0, a.length()+b.length()-1)){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//    }
+
 }
