a480946c7912171d6669bc09fdcd4a4d4ddba683
Augenstern <1024605927@qq.com>
2020-01-10 22:17:52 +0800

    add

--- JavaWeb/Tcp/src/TcpServer.java
+++ JavaWeb/Tcp/src/TcpServer.java
@@ -5,13 +5,9 @@ import java.net.Socket;
 public class TcpServer {
     private static final int PORT = 9999;
     public static void main(String[] args) {
-        try {
-            
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-        }
+        
+		
+		
     }
 
 
