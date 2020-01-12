516a763f8591239690ee5be38940d68102c8f0a1
Augenstern <1024605927@qq.com>
2020-01-12 21:32:45 +0800

    add

--- JavaWeb/maven-test/src/main/java/Util.java
+++ JavaWeb/maven-test/src/main/java/Util.java
@@ -11,22 +11,24 @@ import java.util.Arrays;
 public class Util {
 
     public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
-		
-////        System.out.println(Util.class.getClassLoader().getResource("test.txt").getPath());
 
-//        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
-//        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);      // 是否需要大小写
-//        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);   // 是否要音调
-//        format.setVCharType(HanyuPinyinVCharType.WITH_V);
-//        String[] pinyins1 = PinyinHelper.toHanyuPinyinStringArray(
-//                'a',format);
-//        String[] pinyins2 = PinyinHelper.toHanyuPinyinStringArray(
-//                '只',format);
-//        String[] pinyins3 = PinyinHelper.toHanyuPinyinStringArray(
-//                '和',format);
-//        System.out.println(Arrays.toString(pinyins1));
-//        System.out.println(Arrays.toString(pinyins2));
-//        System.out.println(Arrays.toString(pinyins3));
+//        System.out.println(Util.class.getClassLoader().getResource("test.txt").getPath());
+        
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);      // 是否需要大小写
+        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);   // 是否要音调
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+        String[] pinyins1 = PinyinHelper.toHanyuPinyinStringArray(
+                'a',format);
+        String[] pinyins2 = PinyinHelper.toHanyuPinyinStringArray(
+                '只',format);
+        String[] pinyins3 = PinyinHelper.toHanyuPinyinStringArray(
+                '和',format);
+        System.out.println(Arrays.toString(pinyins1));
+        System.out.println(Arrays.toString(pinyins2));
+        System.out.println(Arrays.toString(pinyins3));
+
+
         char[] inputs = "中华人民共和国".toCharArray();
         System.out.println(inputs);
         String[][] str = new String[inputs.length][];
@@ -34,10 +36,13 @@ public class Util {
         for(char input : inputs){
             str[i++][0] = String.valueOf(input);
         }
-//        for(int j = 0; j < inputs.length; j++){
-//            str[j][] = PinyinHelper.toHanyuPinyinStringArray(inputs[j], format);
-//        }
-//        System.out.println(str);
+        System.out.println(str);
+        for(int j = 0; j < inputs.length; j++){
+            str[j][] = PinyinHelper.toHanyuPinyinStringArray(inputs[j], format);
+        }
+        System.out.println(str);
+
+
         // （1）inputs char[] 转换为String[][]
 //        String[][]
         // （2）String[][]转换为拼音的排列组合：Set<String>
