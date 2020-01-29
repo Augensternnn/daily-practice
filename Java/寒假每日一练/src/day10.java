4545ef4ff1cd5db191e5344234f68bef81846841
Augenstern <1024605927@qq.com>
2020-01-29 13:48:31 +0800

    add

--- Java/寒假每日一练/src/day10.java
+++ Java/寒假每日一练/src/day10.java
@@ -1,6 +1,35 @@
 public class day10 {
     // 8.字符串转换整数(atoi )
-    public int myAtoi(String str) {
+    public static int myAtoi(String str) {
+        if(str=="")
+            return 0;
+        int min = Integer.MIN_VALUE, max = Integer.MAX_VALUE;
+        String s = str.replace(" ","");
+        int i = 0;
+        if((s.charAt(0)!='+')||(s.charAt(0)!='-')){
+            while (s.charAt(0)>='0' || s.charAt(0)<='9'){
+                i++;
+            }
+        }else if(s.charAt(0)>='0' || s.charAt(0)<='9'){
+            while (s.charAt(0)>='0' || s.charAt(0)<='9'){
+                i++;
+            }
+        }else {
+            return 0;
+        }
+        String res = s.substring(0,i+1);
+        int n = Integer.parseInt(res);
+        if(n<min){
+            return min;
+        }else if(n>max){
+            return max;
+        }
+        return n;
+    }
 
+    public static void main(String[] args) {
+        String s = "42";
+        int n = myAtoi(s);
+        System.out.println(n);
     }
 }
