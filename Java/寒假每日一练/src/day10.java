5d2bbedfbf0d5b5ecd647c8efc210ad5d6f0bc34
Augenstern <1024605927@qq.com>
2020-01-30 20:16:26 +0800

    add

--- Java/寒假每日一练/src/day10.java
+++ Java/寒假每日一练/src/day10.java
@@ -1,35 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class day10 {
     // 8.字符串转换整数(atoi )
     public static int myAtoi(String str) {
         if(str=="")
             return 0;
-        int min = Integer.MIN_VALUE, max = Integer.MAX_VALUE;
         String s = str.replace(" ","");
-        int i = 0;
-        if((s.charAt(0)!='+')||(s.charAt(0)!='-')){
-            while (s.charAt(0)>='0' || s.charAt(0)<='9'){
-                i++;
+        if(s=="")
+            return 0;
+        long min=Integer.MIN_VALUE, max=Integer.MAX_VALUE;
+        int index=1;
+        if((s.charAt(0)=='-' || s.charAt(0)=='+') || (s.charAt(0)>='0' && s.charAt(0)<='9')){
+            for(int i=1; i<s.length(); i++){
+                if(s.charAt(i)>='0' && s.charAt(i)<='9')
+                    index++;
+                else break;
             }
-        }else if(s.charAt(0)>='0' || s.charAt(0)<='9'){
-            while (s.charAt(0)>='0' || s.charAt(0)<='9'){
-                i++;
+            long n = Long.decode(s.substring(0,index));
+            if(n < min){
+                return (int)min;
+            }else if(n > max){
+                return (int)max;
+            }else {
+                return (int)n;
             }
         }else {
             return 0;
         }
-        String res = s.substring(0,i+1);
-        int n = Integer.parseInt(res);
-        if(n<min){
-            return min;
-        }else if(n>max){
-            return max;
+    }
+
+    // 34.在排序数组中查找元素的第一个和最后一个位置
+    public static int[] searchRange(int[] nums, int target) {
+        ArrayList list = new ArrayList();
+        int[] arr = new int[2];
+        for(int i = 0; i < nums.length; i++)
+            list.add(nums[i]);
+        if(list.indexOf(target)==-1){
+            arr[0] = -1;
+            arr[1] = -1;
+            return arr;
+        }else {
+            arr[0] = list.indexOf(target);
+            arr[1] = list.lastIndexOf(target);
+            return arr;
         }
-        return n;
     }
 
     public static void main(String[] args) {
-        String s = "42";
-        int n = myAtoi(s);
-        System.out.println(n);
+//        String s = "";
+//        int n = myAtoi(s);
+//        System.out.println(n);
+        int[] nums = {5,6,7,8,9,1};
+
     }
 }
