4cc5ddf9b21b9b0e69ff6b4b18aa6472486eb84d
Augenstern <1024605927@qq.com>
2020-02-01 12:15:38 +0800

    add

--- Java/Collection/Web/多线程/src/ThreadDemo.java
+++ Java/Collection/Web/多线程/src/ThreadDemo.java
@@ -83,8 +83,105 @@ public class ThreadDemo {
     }
 
     @Test   // notify()
-    public void test4(){
-
+    public void test4() throws InterruptedException {
+        Object object = new Object();
+        MyThread2 waitThread = new MyThread2(true,object);
+        MyThread2 notifyThread = new MyThread2(false,object);
+        Thread thread1 = new Thread(waitThread,"wait线程");
+        Thread thread2 = new Thread(notifyThread,"notify线程");
+        thread1.start();
+        Thread.sleep(1000);
+        thread2.start();
+        System.out.println("main方法结束");
     }
-    class  
+
+    @Test   // notifyAll()
+    public void test5() throws InterruptedException {
+        Object object = new Object();
+        MyThread3 waitThread1 = new MyThread3(true,object);
+        MyThread3 waitThread2 = new MyThread3(true,object);
+        MyThread3 waitThread3 = new MyThread3(true,object);
+        MyThread3 notifyThread = new MyThread3(false,object);
+        Thread thread1 = new Thread(waitThread1,"wait线程A");
+        Thread thread2 = new Thread(waitThread2,"wait线程B");
+        Thread thread3 = new Thread(waitThread3,"wait线程C");
+        Thread thread4 = new Thread(notifyThread,"notify线程");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        Thread.sleep(1000);
+        thread4.start();
+        System.out.println("main方法结束");
+    }
+
+}
+class  MyThread2 implements Runnable{   // notify()
+    private boolean flag;
+    private Object obj;
+
+    public MyThread2(boolean flag, Object obj){
+        super();
+        this.flag=flag;
+        this.obj=obj;
+    }
+
+    public void waitMethod(){
+        synchronized (obj){
+            try {
+                while (true){
+                    System.out.println("wait()方法开始.. "+
+                            Thread.currentThread().getName());
+                    obj.wait();
+                    System.out.println("wait()方法结束.. "+
+                            Thread.currentThread().getName());
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void notifyMethod(){
+        synchronized (obj){
+            try {
+                System.out.println("notify()方法开始.. "+
+                        Thread.currentThread().getName());
+                obj.notify();
+                System.out.println("notify()方法结束.. "+
+                        Thread.currentThread().getName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        if(flag)
+            this.waitMethod();
+        else
+            this.notifyMethod();
+    }
+}
+class  MyThread3 extends MyThread2{   // notifyAll()
+    private boolean flag;
+    private Object obj;
+
+    public MyThread3(boolean flag, Object obj){
+        super(flag,obj);
+    }
+
+    public void notifyMethod(){
+        synchronized (obj){
+            try {
+                System.out.println("notify()方法开始.. "+
+                        Thread.currentThread().getName());
+                obj.notifyAll();
+                System.out.println("notify()方法结束.. "+
+                        Thread.currentThread().getName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
