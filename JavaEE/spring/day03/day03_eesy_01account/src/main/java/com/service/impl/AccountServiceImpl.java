db6ead0046a316ac360501644207b7d7454a4725
Augenstern <1024605927@qq.com>
2020-03-24 11:26:28 +0800

    add

--- JavaEE/spring/day03/day03_eesy_01account/src/main/java/com/service/impl/AccountServiceImpl.java
+++ JavaEE/spring/day03/day03_eesy_01account/src/main/java/com/service/impl/AccountServiceImpl.java
@@ -3,21 +3,29 @@ package com.service.impl;
 import com.dao.IAccountDao;
 import com.domain.Account;
 import com.service.IAccountService;
+import com.utils.TransactionManager;
 
 import java.util.List;
 
 /**
  * 账户的业务层实现类
+ *
+ * 事务控制应该都是在业务层
  */
 public class AccountServiceImpl implements IAccountService {
     private IAccountDao accountDao;
+    private TransactionManager tsManager;
+
+    public void setTsManager(TransactionManager tsManager) {
+        this.tsManager = tsManager;
+    }
 
     public void setAccountDao(IAccountDao accountDao) {
         this.accountDao = accountDao;
     }
 
     public List<Account> findAllAccount() {
-        return accountDao.findAllAccount();
+            return accountDao.findAllAccount();
     }
 
     public Account findAccountById(Integer accountId) {
@@ -37,18 +45,18 @@ public class AccountServiceImpl implements IAccountService {
     }
 
     public void transfer(String sourceName, String targetName, Float money) {
-        //1.根据名称查询转出账户
+        //2.1 根据名称查询转出账户
         Account source = accountDao.findAccountByName(sourceName);
-        //2.根据名称查询转入账户
+        //2.2 根据名称查询转入账户
         Account target = accountDao.findAccountByName(targetName);
-        //3.转出账户减钱
+        //2.3 转出账户减钱
         source.setMoney(source.getMoney()-money);
-        //4.转入账户加钱
+        //2.4 转入账户加钱
         target.setMoney(target.getMoney()+money);
-        //5.更新转出账户
+        //2.5 更新转出账户
         accountDao.updateAccount(source);
         int i = 1/0;
-        //6.更新转入账户
+        //2.6 更新转入账户
         accountDao.updateAccount(target);
     }
 }
